3c44ae005d37947990bedc2fbc5f394b8e1c7410
Andrea da Fonseca <me@adf.ninja>
2016-07-12 04:45:23 +0200

    indent

--- src/inner-content.ts
+++ src/inner-content.ts
@@ -1,72 +1,72 @@
 import {
-	Component,
-	ComponentResolver,
-	Directive,
-	ViewContainerRef,
-	Input,
-	Injector,
-	ApplicationRef
+  Component,
+  ViewContainerRef,
+  Injector,
+  ComponentResolver,
+  Directive,
+  Input,
+  ApplicationRef
 } from "@angular/core";
 
 /**
 
-	This component renders an HTML code with inner directives on it.
-	The @Input innerContent receives an array argument, the first array element
-	is the code to be parsed. The second index is an array of Components that
-	contains the directives present in the code.
+  This component render an HTML code with inner directives on it.
+  The @Input innerContent receives an array argument, the first array element
+  is the code to be parsed. The second index is an array of Components that
+  contains the directives present in the code.
 
-	Example:
+  Example:
 
-	<div [innerContent]="[
-		'Go to <a [routerLink]="[Home]">Home page</a>',
-		[RouterLink]
-	]">
+  <div [innerContent]="[
+    'Go to <a [routerLink]="[Home]">Home page</a>',
+    [RouterLink]
+  ]">
 
 **/
 @Directive({
-	selector: '[innerContent]'
+  selector: "[innerContent]",
 })
 export class InnerContent {
 
-	@Input()
-	set innerContent(content){
-		this.renderTemplate(
-			content[0],
-			content[1]
-		)
-	}
+  @Input()
+  set innerContent(content) {
+    this.renderTemplate(
+      content[0],
+      content[1]
+    );
+  }
 
-	constructor(
-		private elementRef: ViewContainerRef,
-		private injector: Injector,
-		private app: ApplicationRef,
-		private resolver:ComponentResolver){
-	}
+  constructor(
+    private elementRef: ViewContainerRef,
+    private injector: Injector,
+    private app: ApplicationRef,
+    private resolver: ComponentResolver) {
+  }
 
-	public renderTemplate(template, directives) {
-		const dynComponent = this.toComponent(template, directives)
-		this.resolver.resolveComponent(
-			dynComponent
-		).then(factory => {
-			const component = factory.create(
-				this.injector, null, this.elementRef.element.nativeElement
-			);
+  public renderTemplate(template, directives) {
+    const dynComponent = this.toComponent(template, directives);
+    this.resolver.resolveComponent(
+      dynComponent
+    ).then(factory => {
+      const component = factory.create(
+        this.injector, null, this.elementRef.element.nativeElement
+      );
 
-			(<any>this.app)._loadComponent(component);
-            component.onDestroy(() => {
-                 (<any>this.app)._unloadComponent(component);
-            });
-            return component;
-		});
-    }
+      (<any>this.app)._loadComponent(component);
+      component.onDestroy(() => {
+        (<any>this.app)._unloadComponent(component);
+      });
+      return component;
+    });
+  }
 
-	private toComponent(template, directives = []) {
-	    @Component({
-			selector: 'gen-node',
-			template: template,
-			directives: directives
-		})
-		class DynComponent {}
-		return DynComponent;
-	}
+  private toComponent(template, directives = []) {
+    @Component({
+      selector: "gen-node",
+      template: template,
+      directives: directives
+    })
+    class DynComponent { }
+    return DynComponent;
+  }
 }
